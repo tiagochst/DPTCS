30ce7985243d339ea7d1ef023a85aa50175284fc
Tiago Chedraoui Silva <tiago@tcs.localhost>
2011-11-30 18:43:56 -0200

    Debut resume

--- MDI224/TP1/jacobi.m
+++ MDI224/TP1/jacobi.m
@@ -1,35 +1,36 @@
-% % % % % % % % % % % % % % % % % % % % % % %
+% % % % % % % % % % % % % % % % % % %
 % xx/12/11
 % Chedraoui Silva,Tiago 
 % Casier: 214 
 % TP1: interpolation par splines cubiques
-% Description: Méthode jacobi pour résoudre 
-%   un système linéaire
-% % % % % % % % % % % % % % % % % % % % % % %
+% Description: Methode jacobi 
+% pour resoudre un systeme lineaire
+% % % % % % % % % % % % % % % % % % %
 
 function x = jacobi(A,b,x0,eps,maxit)
 
-% Entrée
+% Entree
 % A : matrice
 % b : vecteur
 % x0: vecteur d'initialisation 
-% esp: critère de convergence 
-% maxit: nombre maximal d'itérations
+% esp: critere de convergence 
+% maxit: nombre maximal d'iterations
 
 % A in [N X N]
 N = size(A);
 
 % Sortie
 x =  zeros(N);
+
+% Iniatialization sortie
 x = x0;
 
-
-% Décomposition de A: 
+% Decomposition de A: 
 % A = M - K
 % M = D 
 % K = L + U
 
-% création de matrices N X N
+% creation de matrices N X N
 K = zeros(N);
 D = zeros(N);
 
@@ -47,14 +48,28 @@ for i=1:maxit,
 
   xn=inv(D)*(b-K*x);
 
-  % si l'erreur d'approximation est plus petitr
-  % que eps on doit arrêter
+  % sauvegarder les valeurs pour faire
+  % le plot log(ereur) X iteres 
+  printf('NORM %e\n',norm( x - xn ));
+  
+  y(i)=log(norm( x - xn )); 
+  
+  % si l'erreur d'approximation est plus petite
+  % que eps on doit arreter
   if (norm( x - xn ) < eps) 
-    printf('Nombre itérations: %d\n',i);
+    printf('Nombre iterations: %d\n',i);
     break;
   end;
   
-  %sinon je doit répéter l'itération jusqu'à convergence
+  % sinon je doit repeter l'iteration jusqu'a convergence
   x = xn;
-  
+
 end;
+
+% Plot graphic
+h = figure; 
+filename = 'jacobi_graph';
+plot(y);
+xlabel('Iteres');
+ylabel('log(erreur)');
+print(h, '-depsc2', filename);
